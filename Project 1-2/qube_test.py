9186fa89ef1ab41c0beaa0b15e9045e70f486df2
marridG <1687450542@qq.com>
2020-10-13 21:03:15 +0800

    ADD SAMPLE QUBE TEST CODES

---
+++ Project 1-2/qube_test.py
@@ -0,0 +1,21 @@
+"""
+The minimal program that shows the basic control loop on the simulated swing-up.
+"""
+
+import gym
+from quanser_robots import GentlyTerminating
+from quanser_robots.qube import SwingUpCtrl
+
+env = GentlyTerminating(gym.make('Qube-100-v0'))
+
+ctrl = SwingUpCtrl()
+obs = env.reset()
+done = False
+while not done:
+    env.render()
+    act = ctrl(obs)
+    # print(act)
+    obs, _, done, _ = env.step(act)
+    print(obs)
+
+env.close()
