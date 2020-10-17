abd69ac6c980c5e0005695674ac304c7e137a854
jjjaaafff <50798815+jjjaaafff@users.noreply.github.com>
2020-10-17 15:54:45 +0800

    random shooting method for Qube

--- Project 1-2/MPC/MPC-Qube/controller.py
+++ Project 1-2/MPC/MPC-Qube/controller.py
@@ -37,6 +37,22 @@ class MPC(object):
         return optimizer.solution[0]
 
 
+    def get_action(self, state, dynamic_model):
+        self.evaluator.update(state, dynamic_model)
+        obs = np.array([state for _ in range(self.max_itrs)])
+        trajectory_cost_list = np.zeros(self.max_itrs)
+        obs_next_list, act_list = [], []
+        for _ in range(self.horizon):  # tqdm.tqdm(range(self.horizon)):
+            actions = np.array([self.env.action_space.sample() for _ in range(self.max_itrs)])
+            act_list.append(actions)
+            input_data = np.column_stack((obs, actions))
+            _actions_costs = self.evaluator.get_cost(obs, actions)
+            trajectory_cost_list += _actions_costs
+            obs = dynamic_model.predict(input_data)
+            obs_next_list.append(obs)
+        j = np.argmin(trajectory_cost_list)
+        return act_list[0][j]  # 返回一个数组
+
 class Evaluator(object):
     def __init__(self, gamma=0.8):
         self.gamma = gamma
@@ -68,3 +84,22 @@ class Evaluator(object):
         cost = al_mod ** 2 + 5e-3 * al_d ** 2 + 1e-1 * th ** 2 + 2e-2 * th_d ** 2 + 3e-3 * action ** 2
         reward = np.exp(-cost) * 0.02
         return reward
+
+
+    def get_cost(self, obs, action_n):
+        # cos_th, sin_th, cos_al, sin_al, th_d, al_d = obs
+        cos_th = obs[:, 0]
+        cos_al = obs[:, 2]
+        th_d = obs[:, 4]
+        al_d = obs[:, 5]
+        cos_th = np.maximum(cos_th, -1)
+        cos_th = np.minimum(cos_th, 1)
+        cos_al = np.maximum(cos_al, -1)
+        cos_al = np.minimum(cos_al, 1)
+        al = np.arccos(cos_al)
+        th = np.arccos(cos_th)
+        al_mod = al % (2 * np.pi) - np.pi
+        action = action_n[:, 0] * 5
+        cost = al_mod ** 2 + 5e-3 * al_d ** 2 + 1e-1 * th ** 2 + 2e-2 * th_d ** 2 + 3e-3 * action ** 2
+        return cost
+
