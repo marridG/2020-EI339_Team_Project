920dd9c449aa588068c5adf8e25323ba40ba46e9
marridG <1687450542@qq.com>
2020-10-17 15:41:45 +0800

    reformat codes

--- Project 1-2/MPC/MPC-CartPoleSwing/controller.py
+++ Project 1-2/MPC/MPC-CartPoleSwing/controller.py
@@ -23,18 +23,19 @@ class MPC(object):
         :return: (float) optimal action
         """
         self.evaluator.update(state, dynamic_model)
-        optimizer = Hive.BeeHive( lower = [float(self.action_low)] * self.horizon,
-                                  upper = [float(self.action_high)] * self.horizon,
-                                  fun = self.evaluator.evaluate,
-                                  numb_bees = self.numb_bees,
-                                  max_itrs = self.max_itrs,
-                                  verbose=False)
+        optimizer = Hive.BeeHive(lower=[float(self.action_low)] * self.horizon,
+                                 upper=[float(self.action_high)] * self.horizon,
+                                 fun=self.evaluator.evaluate,
+                                 numb_bees=self.numb_bees,
+                                 max_itrs=self.max_itrs,
+                                 verbose=False)
         cost = optimizer.run()
-        #print("Solution: ",optimizer.solution[0])
-        #print("Fitness Value ABC: {0}".format(optimizer.best))
+        # print("Solution: ",optimizer.solution[0])
+        # print("Fitness Value ABC: {0}".format(optimizer.best))
         # Uncomment this if you want to see the performance of the optimizer
-        #Utilities.ConvergencePlot(cost)
+        # Utilities.ConvergencePlot(cost)
         return optimizer.solution[0]
+
 
 class Evaluator(object):
     def __init__(self, gamma=0.8):
@@ -50,16 +51,15 @@ class Evaluator(object):
         rewards = 0
         state_tmp = self.state.copy()
         for j in range(horizon):
-            input_data = np.concatenate( (state_tmp,[actions[j]]) )
+            input_data = np.concatenate((state_tmp, [actions[j]]))
             state_dt = self.dynamic_model.predict(input_data)
             state_tmp = state_tmp + state_dt[0]
             rewards -= (self.gamma ** j) * self.get_reward(state_tmp, actions[j])
         return rewards
 
     # need to change this function according to different environment
-    def get_reward(self,obs, action_n):
+    def get_reward(self, obs, action_n):
         x, sin_th, cos_th, x_dot, theta_dot = obs
         cos_th = min(max(cos_th, -1), 1)
         reward = -cos_th + 1
         return reward
-
